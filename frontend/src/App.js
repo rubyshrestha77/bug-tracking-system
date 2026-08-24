9e7006a92ea8e0bc02ece5ed52a2b5a41946a0a1
rubyshrestha77 <rubyshrestha77@gmail.com>
2026-08-24 20:09:45 +1000

    BTS-10: add Register component with role selection and hanlde errors

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
